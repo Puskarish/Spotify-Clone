1659f575e997ac93e90f407be2b8c2635f0fab7e
Puskar Raj <puskarraj4006@gmail.com>
2026-06-09 18:38:06 +0530

    java

---
+++ js/script.js
@@ -0,0 +1,115 @@
+console.log('Lets write JavaScript');
+let currentSong = new Audio()
+
+function secondsToMinutesSeconds(seconds){
+    if(isNaN(seconds) || seconds < 0){
+        return "00:00"
+    }
+
+    const minutes = Math.floor(seconds/60)
+    const remainingSeconds = Math.floor(seconds % 60)
+
+    const formattedMinutes = String(minutes).padStart(2, '0')
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0')
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/%5Csongs%5C")[1])
+        }
+    }
+    return songs;
+    
+    
+}
+
+const playMusic = (track, pause = false)=>{
+    currentSong.src = "/%5Csongs%5C" + track
+    if(!pause){
+        currentSong.play()
+        play.src = "img/pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00/00:00"
+}
+
+
+
+
+async function main(){
+
+    // Get the list of all the songs
+    let songs = await getSongs()
+    playMusic(songs[0], true)
+
+    // Show all the songs in the library
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + 
+                        `<li>
+                            <img class="invert" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")} </div>
+                                <div> Puskar </div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/play.svg" alt="">
+                            </div>    
+                        </li>`
+    }
+
+    // Listen for timeupdate event
+    currentSong.addEventListener("timeupdate", ()=>{
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%"
+    })
+
+    // Add an event listen to seekbar
+    document.querySelector(".seekbar").addEventListener("click", e=>{
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100
+        document.querySelector(".circle").style.left = percent + "%"
+        currentSong.currentTime = ((currentSong.duration) * percent) / 100
+    })
+
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+        
+    })
+
+    // Attach an event listener to play, next, previous
+    play.addEventListener("click", ()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "img/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "img/play.svg"
+        }
+    })
+
+    // previous.addEventListener("click", ()=>{
+    //     currentSong.pause()
+    //     console.log("Previous clicked")
+    //     let index = songs.indexOf(currentSong.src.split)
+    // })
+
+}
+
+main()
+
+
